704b5856f305dc1a472552baa1a622799058ee5e
gretaivan <greta.ivan91@gmail.com>
2021-08-11 15:32:24 +0100

    adds another test and test to test the exception behaviour

--- src/test/CafeTest.java
+++ src/test/CafeTest.java
@@ -12,12 +12,41 @@ public class CafeTest {
 
 	@Test
 	public void shouldBrewEspresso() {
+		//Given
 		Cafe cafe = new Cafe();
 		cafe.restockBeans(7);
+		
+		//When 
 		Coffee coffee = cafe.brew(CoffeeType.Espresso);
 		
+		//Then
 		assertEquals(7, coffee.getBeans());
 		assertEquals(0, coffee.getMilk());
 		assertEquals(CoffeeType.Espresso, coffee.getType());
+	}
+	
+	@Test 
+	public void shoulUpdateStockLevels(){
+		//Given
+		Cafe cafe = new Cafe();
+		cafe.restockBeans(7);
+		
+		//When
+		cafe.brew(CoffeeType.Espresso); 
+		
+		//Then
+		assertEquals(0, cafe.getBeansInStock());
+	}
+	
+	@Test 
+	public void shouldRequireBeensToBrewEspresso() {
+		//G
+		Cafe cafe = new Cafe(); 
+		
+		//T -- assert that exception was thrown
+		assertThrows(IllegalStateException.class, () -> {
+			//W - deliberately not filled to throw an exception 
+			cafe.brew(CoffeeType.Espresso);
+		});
 	}
 }
