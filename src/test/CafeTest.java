22064bde52a12c3b3c90552432740f9251cab109
gretaivan <greta.ivan91@gmail.com>
2021-08-11 15:59:46 +0100

    refactors code to a better practice and adds annotations

--- src/test/CafeTest.java
+++ src/test/CafeTest.java
@@ -2,25 +2,49 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
 import main.Cafe;
 import main.Coffee;
 import main.CoffeeType;
 
+
+
+
 public class CafeTest {
 
+	public static final int ESPRESSO_BEANS_WEIGHT = 7;
+	
+	Cafe cafe;
+	
+	@BeforeEach
+	public void setup() {
+		cafe = new Cafe();
+		System.out.println("before each");
+	}
+	
+	@AfterEach
+	public void teardown() {
+		System.out.println("after each");
+	}
+	
+	@AfterAll
+	public static void end() {
+		System.out.println("Test end");
+	}
+	
 	@Test
 	public void shouldBrewEspresso() {
 		//Given
-		Cafe cafe = new Cafe();
-		cafe.restockBeans(7);
+		cafe.restockBeans(ESPRESSO_BEANS_WEIGHT);
 		
 		//When 
 		Coffee coffee = cafe.brew(CoffeeType.Espresso);
 		
 		//Then
-		assertEquals(7, coffee.getBeans());
+		assertEquals(ESPRESSO_BEANS_WEIGHT, coffee.getBeans());
 		assertEquals(0, coffee.getMilk());
 		assertEquals(CoffeeType.Espresso, coffee.getType());
 	}
@@ -28,20 +52,18 @@ public class CafeTest {
 	@Test 
 	public void shoulUpdateStockLevels(){
 		//Given
-		Cafe cafe = new Cafe();
-		cafe.restockBeans(7);
+		cafe.restockBeans(ESPRESSO_BEANS_WEIGHT);
 		
 		//When
 		cafe.brew(CoffeeType.Espresso); 
 		
 		//Then
-		assertEquals(0, cafe.getBeansInStock());
+		assertEquals( 1, cafe.getBeansInStock(), "I am a diagnostic message here I should say: Does not update the bean stock correctly!");
 	}
 	
 	@Test 
 	public void shouldRequireBeensToBrewEspresso() {
 		//G
-		Cafe cafe = new Cafe(); 
 		
 		//T -- assert that exception was thrown
 		assertThrows(IllegalStateException.class, () -> {
